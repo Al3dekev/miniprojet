88537f711cfa55aaf26c7e2d2c8d98960cbcedd0
LoicLab <33249265+LoicLab@users.noreply.github.com>
2018-02-20 15:47:46 +0100

    Revert "remove file launcher"
    This reverts commit e88b3f3a203161cc84de8438863d6819e437d034.

---
+++ script/Launcher/ApplicationLoader.js
@@ -0,0 +1,47 @@
+/*
+* Lauchment Class of the P2P chat application.
+* The World have to start in a first way.
+*
+*
+*/
+
+function ApplicationLoader(){
+
+	/* No Attributs */
+
+
+	// Constructeur
+	this._construct = function () {
+        var electronLoaded = new ElectronInit(600,400);
+
+	};
+
+	this.creatUser = function(){
+		var $ = document.querySelector.bind(document);
+		var input = document.querySelector(".form-control");
+		var bouton = document.querySelector(".bouton");
+		bouton.onclick = function() {
+				var name = false;
+				name = input.value;
+				if (name == false) {
+					alert("Veuillez mettre un nom !");
+				} else {
+					var user = new Entity();
+					user.setName(name);
+					localStorage.setItem("pseudo", name);
+					document.location.href = 'pageaccueil.html';
+				}
+
+	}
+
+	// var bouton = $(".creatUserForm .bouton");
+  // bouton.onclick = function() {
+  //   var elem = $(".creatUserForm")
+  //   elem.parentNode.removeChild(elem);
+  // }
+
+	this.welcome = function(){
+
+	}
+}
+}
