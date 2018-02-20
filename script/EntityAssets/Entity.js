1d3f2e18cb660de01760893a6b192188a798a7f6
Al3dekev <q.basset@icloud.com>
2018-02-20 18:28:33 +0100

    Revert "MaJ correctrice 1.1.0"
    This reverts commit 3dca9b82140f54d7504a3f64fb135ea18236013b.

---
+++ script/EntityAssets/Entity.js
@@ -0,0 +1,37 @@
+function Entity(){
+  this.name;
+  this.index;
+  this.type;
+
+
+  
+  // LISTAGE GET/SET
+ 
+  this.getName = function(){
+    return this.name;
+  }
+
+  this.setName = function(name){
+    this.name = name;
+  }
+
+  this.getIndex = function(){
+    return this.index;
+  }
+
+  this.setIndex = function(name){
+    this.index = index;
+  }
+
+  this.getType = function(){
+    return this.type;
+  }
+
+  this.setType = function(type){
+    this.type = type;
+  }  
+  
+  this.isOnline = function(){
+    return true;
+  }
+}
